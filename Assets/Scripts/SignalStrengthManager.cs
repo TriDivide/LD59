2063ab0bc479a97657dd4090b146e7f538339fee
TriDivide <liamsmitha@gmail.com>
2026-04-18 18:03:30 +0100

    - added the logic for calculating the distance between the player and home base.

---
+++ Assets/Scripts/SignalStrengthManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SignalStrengthManager : MonoBehaviour {
+
+
+    private GameObject homeBase, player;
+
+    public float distance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        homeBase = GameObject.FindGameObjectsWithTag("Base")[0];
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 difference = new Vector2(homeBase.transform.position.x - player.transform.position.x, homeBase.transform.position.y - player.transform.position.y);
+
+        float d = Mathf.Sqrt(Mathf.Pow(difference.x, 2f) + Mathf.Pow(difference.y, 2f));
+
+        distance = Mathf.Floor(d);
+    }
+}
