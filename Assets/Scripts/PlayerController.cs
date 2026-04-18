9955d56beebc84eb51bda7ab26f03f0a07a2fcea
TriDivide <liamsmitha@gmail.com>
2026-04-18 15:16:07 +0100

    - added basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("Params")]
+    [SerializeField] private float acceleration = 10f;
+    [SerializeField] private float maxVelocity = 10f;
+    [SerializeField] private float rotationSpeed = 180f;
+
+
+
+    private bool isAlive = true;
+    private bool isAccelerating = false;
+    private bool isReversing = false;
+
+    private bool isBraking = false;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { 
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {  
+        if (isAlive) {
+            HandleAcceleration();
+            HandleRotation();
+        }
+    }
+
+
+    private void FixedUpdate() {
+        if (isAlive && isAccelerating) {
+            rb.AddForce(acceleration * transform.up);
+            rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxVelocity);
+        }
+
+        if (isAlive && isReversing) {
+            rb.AddForce(-(acceleration/2) * transform.up);
+            rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxVelocity/2);
+        }
+
+        if (isAlive && isBraking && (!isAccelerating || !isReversing)) {
+            rb.linearVelocity *= 0.95f;
+            rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxVelocity);
+
+            print(rb.linearVelocity);
+        }
+
+
+    }
+
+
+    private void HandleAcceleration() {
+        isAccelerating = Input.GetKey(KeyCode.UpArrow);
+        isReversing = Input.GetKey(KeyCode.DownArrow);
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleRotation() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(rotationSpeed * Time.deltaTime * transform.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(-rotationSpeed * Time.deltaTime * transform.forward);
+        }
+    }
+}
